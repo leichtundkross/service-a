4f64878518db34cb88784d9b22f2f1934da327a0
pheckelsmueller <philipp.heckelsmueller@senacor.com>
2023-12-14 10:09:57 +0100

    Name geandert

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.get('/hello/:name', async (req, res) => {
   // TODO: this should point to service B
   const response = await axios.get('https://timezoneapi.io/api/timezone/?Europe/Berlin&token=aleNAMdkOzvJjkMmlmzw')
 
-  res.send(`Hola! ${req.params.name}. The date is ${response.data.data.datetime.date_time}`)
+  res.send(`Servus! ${req.params.name}. The date is ${response.data.data.datetime.date_time}`)
 
 })
 
